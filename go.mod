e44dfcb019c09ab69e3d78fbd26738f35177f482
Takumi Kawaguchi <tcgds4@gmail.com>
2021-07-18 19:46:47 +0900

    [update] chromedriverをGOライブラリで呼び出せるか動作確認

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module takumi-kawaguchi/instascraper
 
 go 1.13
+
+require (
+	github.com/onsi/gomega v1.14.0 // indirect
+	github.com/sclevine/agouti v3.0.0+incompatible
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/sclevine/agouti"
+)
 
 func main() {
-	fmt.Println("hello, world.")
+	driver := agouti.ChromeDriver()
+	defer driver.Stop()
+
+	if err := driver.Start(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+
+	page, err := driver.NewPage()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+
+	page.Navigate("https://kakaku.com/")
+	fmt.Println(page.Title())
 }
