16df8a71693769027392b8525542d193373d5757
Takumi Kawaguchi <tcgds4@gmail.com>
2021-07-24 23:55:37 +0900

    [update] アカウント名を標準入力で受け取れるようにした

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 
 const (
 	instagramUrl  = "https://www.instagram.com/[targetAccount]/?hl=ja"
-	targetAccount = ""
 	replaceString = "[targetAccount]"
 
 	imageDirectoryPath = "C:\\Users\\kawaguchi_takumi\\workspace\\instascraperStorage\\"
@@ -23,7 +22,11 @@ const (
 )
 
 func main() {
-	url := "https://www.instagram.com/[test]/?hl=ja"
+	fmt.Print("input account name: ")
+	var inputAccount string
+	fmt.Scan(&inputAccount)
+
+	url := "https://www.instagram.com/" + inputAccount + "/?hl=ja"
 	directoryName := time.Now().Format(timeToStringFormat)
 	directoryPath := imageDirectoryPath + directoryName
 	if err := os.Mkdir(directoryPath, 0777); err != nil {
@@ -55,24 +58,24 @@ func main() {
 	contents, _ := goquery.NewDocumentFromReader(reader)
 
 	contents.Find(".KL4Bh > img").Each(func(i int, s *goquery.Selection) {
-		img, _ := s.Attr("src")
-		res, err := http.Get(img)
-		if err != nil {
-			panic(err)
-		}
-		defer res.Body.Close()
+		if i <= 3 {
+			img, _ := s.Attr("src")
+			res, err := http.Get(img)
+			if err != nil {
+				panic(err)
+			}
+			defer res.Body.Close()
 
-		fileName := directoryPath + "\\" + targetAccount + strconv.Itoa(i) + ".jpg"
-		file, err := os.Create(fileName)
-		if err != nil {
-			panic(err)
-		}
-		defer file.Close()
+			fileName := directoryPath + "\\" + inputAccount + strconv.Itoa(i) + ".jpg"
+			file, err := os.Create(fileName)
+			if err != nil {
+				panic(err)
+			}
+			defer file.Close()
 
-		io.Copy(file, res.Body)
-		fmt.Println("download file: ", i)
-		time.Sleep(1 * time.Second)
+			io.Copy(file, res.Body)
+			fmt.Println("download file: ", i)
+			time.Sleep(1 * time.Second)
+		}
 	})
-
-	fmt.Println("FIN")
 }
